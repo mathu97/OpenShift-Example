2e789c1e83cc16df251b806c00d0983211e257ab
Mathusan Selvarajah <mselvara@redhat.com>
2018-06-21 10:54:50 -0400

    Adding create table endpoint

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 var expresss = require('express');
+var mysql = require('mysql');
+
 var app = express();
 
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    port: "3306",
+    user: "user1",
+    password: "mypa55",
+    database: "testdb"
+});
+
+con.connect(function(err){
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+app.post('/createtable', function(req, res) {
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+      });
+});
 
 app.listen(3000, () => {
     console.log("Server Listening On Port 3000")
