245ed0beb36d7d316029018bdd2caadab8f3ea20
Mathusan Selvarajah <mselvara@redhat.com>
2018-06-29 11:55:09 -0400

    adding cors to front-end

--- front-end/frontend-app.js
+++ front-end/frontend-app.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var path = require('path')
+var cors = require('cors');
 
 var app = express();
 
+var viewDest = String(__dirname)
+app.use(express.static(viewDest + '/'));
+
+app.use(cors());
+
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + '/main.html'))
-})
+    res.sendFile(viewDest + '/main.html')
+});
 
 app.listen(4000, () => {
     console.log("Front-end hosted On Port 4000")
